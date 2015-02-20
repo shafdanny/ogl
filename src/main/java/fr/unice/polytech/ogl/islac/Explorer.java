79f8399d6fca2cd8a7bbf0229168fd06aebb87c8
Shafiq Daniel BIN SABARI <bin.sabari.shafiq.daniel@etu.unice.fr>
2015-02-20 11:21:30 +0100

    First version of the bot

---
+++ src/main/java/fr/unice/polytech/ogl/islac/Explorer.java
@@ -0,0 +1,21 @@
+package fr.unice.polytech.ogl.islxx;
+
+import eu.ace_design.island.bot.*;
+
+public class Explorer implements IExplorerRaid {
+
+    @Override
+	public void initialize(String context) {
+		throw new UnsupportedOperationException("IExplorerRaid::initialize is not yet implemented");
+	}
+
+    @Override
+	public String takeDecision() {
+		throw new UnsupportedOperationException("IExplorerRaid::takeDecision is not yet implemented");
+	}
+
+    @Override
+	public void acknowledgeResults(String results) {
+		throw new UnsupportedOperationException("IExplorerRaid::acknowledgeResults is not yet implemented");
+	}
+}
