c5d879caf2bb29645194b3295d9068f459c0fcc3
Cyrus Boumedine <bc407029@etu.unice.fr>
2015-03-06 11:02:20 +0100

    ISLAC-7

--- src/main/java/fr/unice/polytech/ogl/islac/Methods.java
+++ src/main/java/fr/unice/polytech/ogl/islac/Methods.java
@@ -7,6 +7,11 @@ public class Methods {
 		return "{ \"action\": \"stop\" }";
 	}
 	
+	public void extraireInfo(String context)
+	{
+		
+	}
+	
 	public String land(String creek_id, int number){
 		return "{ \"action\": \"land\", \"parameters\": {\"creek\":" + creek_id + ", \"people\":" +	number + "}}";
 	}
