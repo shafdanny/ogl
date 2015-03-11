638aaab212726d7162710a85299757f32e72b2b5
Cyrus Boumedine <bc407029@etu.unice.fr>
2015-03-11 16:00:54 +0100

    ISLAC-27 Ajout des packages JSON manquant et d'un exmple dans la classe Test

--- src/main/java/fr/unice/polytech/ogl/islac/Test.java
+++ src/main/java/fr/unice/polytech/ogl/islac/Test.java
@@ -1,13 +1,37 @@
 package fr.unice.polytech.ogl.islac;
 
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+import java.lang.*;
+
 public class Test {
 
-	/**
-	 * @param args
-	 */
-	//public static void main(String[] args) {
+
+	public static void main(String[] args) 
+	   {
+		 
+	  
+	      try {
+	    	  JSONParser parser=new JSONParser();
+		      String s = "{\"creek\":\"creek_id\",\"budget\": 600,\"men\": 50,\"objective\": [ { \"resource\": \"WOOD\", \"amount\": 600 }]}"; 
+		      JSONObject obj = (JSONObject)parser.parse(s);
+		      System.out.println(obj.get("creek"));
+		      System.out.println(obj.get("budget"));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	      
+	      
+	   }
+}
+			
+
 		
 
-	}
+
+
+	
 
 
