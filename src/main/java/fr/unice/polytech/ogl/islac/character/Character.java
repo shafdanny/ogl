e4ec676e75e1616c9d60e6602b70cb01e025aa16
Mathieu Stackler <stackler.mathieu@etu.unice.fr>
2015-03-13 11:16:17 +0100

    Résolution bug Git

--- src/main/java/fr/unice/polytech/ogl/islac/character/Character.java
+++ src/main/java/fr/unice/polytech/ogl/islac/character/Character.java
@@ -1,5 +1,38 @@
 package fr.unice.polytech.ogl.islac.character;
 
 public class Character {
+	
+	private int pa;
+	private int nbTotal;
+	private int nbLand;
+	
 
+	public void setPa(int pa) {
+		this.pa=pa;
+	}
+	
+	public void setNbLand(int nb){
+		this.nbLand=nb;
+	}
+	
+	public void setNbTotal(int nb){
+		this.nbTotal=nb;
+	}
+	
+	public int getPa(){
+		return pa;
+	}
+	
+	public int getNbLand(){
+		return nbLand;
+	}
+	
+	public int getNbTotal(){
+		return nbTotal;
+	}
+	
+	public int getNbBoat(){
+		return getNbLand()-getNbTotal();
+	}
 }
+	
