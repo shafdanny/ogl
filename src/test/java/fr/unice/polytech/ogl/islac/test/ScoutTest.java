9d3069bd469e27fbc1358a76824243ca78bc49d1
Mathieu Stackler <stackler.mathieu@etu.unice.fr>
2015-03-20 14:30:33 +0100

    ISLAC-56

--- src/test/java/fr/unice/polytech/ogl/islac/test/ScoutTest.java
+++ src/test/java/fr/unice/polytech/ogl/islac/test/ScoutTest.java
@@ -1,6 +1,18 @@
 package fr.unice.polytech.ogl.islac.test;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import fr.unice.polytech.ogl.islac.action.Action;
+import fr.unice.polytech.ogl.islac.action.Scout;
+
 public class ScoutTest {
 
+	@Test public void readScout(){
+		
+		String s = "{\"status\":\"OK\", \"cost\": 8,\"extras\": {\"resources\": [\"WOOD\", \"FUR\", \"FLOWER\"], \"altitude\" : -23}}";
+		Action test= new Action();
+		test.readScout(s);
+	}
 }
