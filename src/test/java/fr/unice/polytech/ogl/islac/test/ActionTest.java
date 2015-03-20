0c35974bcab65a685df3637aa61e6661b77c2246
Thomas Moras <moras.thomas@gmail.com>
2015-03-20 12:20:31 +0100

    ajout des tests et du package contenant les tests

---
+++ src/test/java/fr/unice/polytech/ogl/islac/test/ActionTest.java
@@ -0,0 +1,8 @@
+package fr.unice.polytech.ogl.islac.test;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ActionTest {
+	 @Test public void read() { 
+	 }
+}
---
+++ src/test/java/fr/unice/polytech/ogl/islac/test/Character1Test.java
@@ -0,0 +1,21 @@
+package fr.unice.polytech.ogl.islac.test;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import fr.unice.polytech.ogl.islac.character.Character1;
+
+public class Character1Test {
+	
+	private Character1
+	
+	c = new Character1();
+	
+	@Test public void addPa(int pa){
+		
+	}
+
+}
+
+	
+	
