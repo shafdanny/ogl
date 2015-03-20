20cf9dbeaa18577779a2748aa7062bc37e2e33e6
Thomas Moras <moras.thomas@gmail.com>
2015-03-20 13:57:25 +0100

    ISLAC-49 test de addPa Character1

--- src/test/java/fr/unice/polytech/ogl/islac/test/Character1Test.java
+++ src/test/java/fr/unice/polytech/ogl/islac/test/Character1Test.java
@@ -8,22 +8,12 @@ import fr.unice.polytech.ogl.islac.character.Character1;
 
 public class Character1Test {
 	
-	//private Character1
-	
-	
-
-	@Before public void setUp() {
-		Character1 c = new Character1();
-		Character1 expected = new Character1();
-		Character1 result = new Character1();
-	} 
-	
-	@Test public void addPa(int pa){
-	Character1 c = null;
+	@Test public void addPa(){
+	Character1 c = new Character1();
+	Character1 expected = new Character1();
+	Character1 result = new Character1();
 	c.setPa(5);
-	Character1 expected = null;
 	expected.setPa(15);
-	Character1 result = null;
 	result.setPa(c.getPa()+10);
 	assertEquals(expected.getPa(), result.getPa());
 	}
