b4d6beb7c1f9cb639f0a1898295e9227d20fb591
Mathieu Stackler <stackler.mathieu@etu.unice.fr>
2015-03-20 11:24:46 +0100

    ISLAC-47 changements dans le packages action

--- src/main/java/fr/unice/polytech/ogl/islac/action/Scout.java
+++ src/main/java/fr/unice/polytech/ogl/islac/action/Scout.java
@@ -1,8 +1,10 @@
 package fr.unice.polytech.ogl.islac.action;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import java.util.*;
 
 public class Scout extends Action {
 	
@@ -14,8 +16,16 @@ public class Scout extends Action {
 	    	  JSONParser parser=new JSONParser();		  
 		      JSONObject obj = (JSONObject)parser.parse(s);
 		      JSONObject obj1= (JSONObject)obj.get("extras");
+		      JSONArray array= (JSONArray)obj1.get("ressources");
 		      
 		      int pa=(int)obj.get("cost");
+		      ArrayList<String> ressources = new ArrayList<String>();
+		      for (int k=0; k<array.size(); k++)
+		      {
+		    	  ressources.add((String)array.get(k));
+		      }
+		      int altitude=(int)obj1.get("altitude");
+		      
 		      
 		      c.addPa(pa);
 		      
