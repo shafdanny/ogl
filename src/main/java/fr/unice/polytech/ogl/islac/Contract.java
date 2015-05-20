e24b7859716f65e317af7583432e5c96d0073b5e
Mathieu Stackler <stackler.mathieu@etu.unice.fr>
2015-05-20 16:04:34 +0200

    ajout de la classe Contract

---
+++ src/main/java/fr/unice/polytech/ogl/islac/Contract.java
@@ -0,0 +1,10 @@
+package fr.unice.polytech.ogl.islac;
+
+import eu.ace_design.island.bot.*;
+import org.json.*;
+import fr.unice.polytech.ogl.islac.character.Character1;
+
+public class Contract {
+
+	
+}
