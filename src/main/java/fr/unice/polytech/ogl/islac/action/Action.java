a0e65ef0443b46d58bc1798abed0203110940aeb
Cyrus Boumedine <bc407029@etu.unice.fr>
2015-06-02 22:04:35 +0200

    activation objectif secondaires

--- src/main/java/fr/unice/polytech/ogl/islac/action/Action.java
+++ src/main/java/fr/unice/polytech/ogl/islac/action/Action.java
@@ -175,7 +175,7 @@ public class Action {
 			    		  a.getC().getPrimaryObjectives().addAll(plank.getResourceNeededToTransform());			    		  
 			    	  }
 			    	  
-			    	/* if(nom.equals("GLASS")){
+			    	 if(nom.equals("GLASS")){
 			    		 
 			    		 if(quantity*80<pa)
 			    		 {
@@ -185,8 +185,8 @@ public class Action {
 			    		  a.getC().getPrimaryObjectives().addAll(glass.getResourceNeededToTransform());
 			    		 }
 			    	 }
-			    	  */
-			    	 /* if(nom.equals("INGOT")){
+			    	  
+			    	  if(nom.equals("INGOT")){
 			    		  if(quantity*55<pa)
 			    		  {
 				    		  SecondaryRessources ingot = new SecondaryRessources(nom, quantity);			    		  
@@ -194,7 +194,7 @@ public class Action {
 				    		  a.getC().getPrimaryObjectives().addAll(ingot.getResourceNeededToTransform());
 			    	  
 			    		  }
-			    	  }*/
+			    	  }
 			    	  
 			    	  if(nom.equals("LEATHER")){
 			    		  if(quantity*40<pa)
