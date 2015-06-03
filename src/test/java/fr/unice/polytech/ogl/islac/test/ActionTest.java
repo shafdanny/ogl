a8f4f8b7af1c325023b8fd9ebe78ea394e5aa332
Mathieu Stackler <stackler.mathieu@etu.unice.fr>
2015-06-03 15:00:26 +0200

    Suppression classes inutiles

--- src/test/java/fr/unice/polytech/ogl/islac/test/ActionTest.java
+++
@@ -1,8 +0,0 @@
-package fr.unice.polytech.ogl.islac.test;
-import static org.junit.Assert.*;
-import org.junit.Test;
-
-public class ActionTest {
-	// @Test public void read() { 
-	// }
-}
--- src/test/java/fr/unice/polytech/ogl/islac/test/MapTest.java
+++
@@ -1,6 +0,0 @@
-package fr.unice.polytech.ogl.islac.test;
-import static org.junit.Assert.*;
-
-public class MapTest {
-
-}
